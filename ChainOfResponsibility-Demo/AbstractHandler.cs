d2a47db90a403027f1f3c6a77fe13eb050f255c6
Magesh <tkmagesh77@gmail.com>
2019-12-10 14:05:04 +0800

    Chain Of Responsibility

---
+++ ChainOfResponsibility-Demo/AbstractHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace ChainOfResponsibility_Demo
+{
+    // The default chaining behavior can be implemented inside a base handler
+    // class.
+    abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            this._nextHandler = handler;
+
+            // Returning a handler from here will let us link handlers in a
+            // convenient way like this:
+            // monkey.SetNext(squirrel).SetNext(dog);
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (this._nextHandler != null)
+            {
+                return this._nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibility-Demo/IHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace ChainOfResponsibility_Demo
+{
+    //Chain Of Responsibility
+
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+
+        object Handle(object request);
+    }
+}
