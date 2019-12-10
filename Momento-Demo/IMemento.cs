f2d0122c8394bc8b7e36a5040707478cbb0e3722
Magesh <tkmagesh77@gmail.com>
2019-12-10 16:27:39 +0800

    momento-demo

---
+++ Momento-Demo/IMemento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Momento_Demo
+{
+    // The Memento interface provides a way to retrieve the memento's metadata,
+    // such as creation date or name. However, it doesn't expose the
+    // Originator's state.
+    public interface IMemento
+    {
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+}
